fd6abb42472cbd0e074ea0106d0e8af635350137
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-10 18:28:15 -0300

    dando comportamento para o botao jogar e utilizando o Random()

--- Sorteio/app/src/main/java/com/sinngjpeg/sorteio/MainActivity.kt
+++ Sorteio/app/src/main/java/com/sinngjpeg/sorteio/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun cliqueBotao(view : View) {
+    fun cliqueBotao(view: View) {
         Log.i("BOTAO", "Botao pressionado")
+
+        var texto = findViewById(R.id.text_sorteio) as TextView
+
+        var numeroDaSorte = Random.nextInt(11)
+        texto.setText("Numero da sorte é  $numeroDaSorte")
 
     }
 }
