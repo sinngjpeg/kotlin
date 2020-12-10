e3701234de73705c228c53fae79b3144d225579a
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-10 19:19:43 -0300

    hellomundo, variaveis

---
+++ exercicios-kotlin-iniciante/hellomundo.kt
@@ -0,0 +1,9 @@
+
+
+
+fun main(args: Array<String>) {
+
+	println("hello porra")
+}
+
+
---
+++ exercicios-kotlin-iniciante/variaveis.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    
+    var usuario = "Jamilton Damasceno"
+    usuario = "Jose"
+    println(usuario)
+    
+    val pi = 3.14
+    println(pi)
+    
+    var salario = 4200
+    var bonus = 514
+    var total = salario + bonus 
+    println( "O salario é: $total")
+
+    
+}
