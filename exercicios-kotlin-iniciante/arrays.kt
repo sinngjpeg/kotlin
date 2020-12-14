c84ef4d9b23c1d3a7df5cec96960b5fafa6fa16a
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-14 15:31:53 -0300

    arrays

---
+++ exercicios-kotlin-iniciante/arrays.kt
@@ -0,0 +1,10 @@
+
+fun main(args: Array<String>) { 
+
+    var nomes = arrayOf("Jamilton", "Ingrid", "Leticia", "Caue", "Regis")
+	var numero = arrayOf(10,20,30)
+    
+    nomes[0] = "Pedro"
+    println(nomes[0])
+    println(numero[1])
+}
--- exercicios-kotlin-iniciante/variaveis.kt
+++ exercicios-kotlin-iniciante/variaveis.kt
@@ -12,5 +12,10 @@ fun main(args: Array<String>) {
     var total = salario + bonus 
     println( "O salario é: $total")
 
+    var decimal = 3.5
+    var float = 3.5f
+    var long = 653434244543
+    var int = 4342
+    var boolean = true 
+
     
-}
